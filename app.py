756e9bf48a27411ef4e30380f4a0ac2dd16a5142
Johann Pérez <johann.perez@nareia.com.uy>
2024-12-02 02:05:11 +0000

    Hello world

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello_world():
+    return "hello world"
